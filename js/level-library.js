6b8fed2fdd98dcfc46e89a88d4036d5efec07883
kszlab <szabo.krisztian@vilcad.hu>
2026-09-23 09:53:31 +0200

    v0.12.45 mark library as Difficulty Model v2

--- js/level-library.js
+++ js/level-library.js
@@ -1,4 +1,4 @@
-/* GGrid v0.12.43 - pre-generated Level Library v2 test loader */
+/* GGrid v0.12.45 - Level Factory v2 pre-generated library loader */
 const LevelLibrary=(()=>{
  const dm={U:'up',D:'down',L:'left',R:'right'};
  let levels=null,cursor=new Map();
@@ -7,7 +7,7 @@ const LevelLibrary=(()=>{
   levels=GGRID_TEST_LEVELS_V2.map(r=>{
    const objects=r.o.map((p,i)=>({id:p[0]==='B'?'ball1':p[0]==='K'?'b'+i:'w'+i,type:p[0]==='B'?'ball':p[0]==='K'?'brick':'wall',x:p[1],y:p[2],cells:p[3].map(c=>({x:c[0],y:c[1]})),glueEdges:[],glued:p[3].length>1}));
    const glueCount=objects.filter(o=>o.type==='brick'&&o.cells.length>1).length;
-   return{format:'ggrid-level',formatVersion:2,levelId:r.id,rulesVersion:1,requires:{features:['core.movement','core.exit','object.ball','object.rigid-body','object.wall']},board:{width:r.w,height:r.h,exit:{dir:dm[r.e[0]],x:r.e[1],y:r.e[2]}},initialResources:{freeze:0},analysis:{testDifficultyClass:r.d,rawDifficulty:r.raw,solution:[...r.sol].map(x=>dm[x])},_state:{width:r.w,height:r.h,exit:{dir:dm[r.e[0]],x:r.e[1],y:r.e[2]},moves:0,won:false,glueCount,brickCount:objects.filter(o=>o.type==='brick').length,wallCount:objects.filter(o=>o.type==='wall').length,objects}};
+   return{format:'ggrid-level',formatVersion:2,levelId:r.id,rulesVersion:1,requires:{features:['core.movement','core.exit','object.ball','object.rigid-body','object.wall']},board:{width:r.w,height:r.h,exit:{dir:dm[r.e[0]],x:r.e[1],y:r.e[2]}},initialResources:{freeze:0},analysis:{testDifficultyClass:r.d,rawDifficulty:r.raw,difficultyModelVersion:2,solution:[...r.sol].map(x=>dm[x])},_state:{width:r.w,height:r.h,exit:{dir:dm[r.e[0]],x:r.e[1],y:r.e[2]},moves:0,won:false,glueCount,brickCount:objects.filter(o=>o.type==='brick').length,wallCount:objects.filter(o=>o.type==='wall').length,objects}};
   });
   return levels;
  }
